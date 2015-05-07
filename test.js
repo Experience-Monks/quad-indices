a2cced0f065813d6cb9190ea47051dbab27303c8
mattdesl <dave.des@gmail.com>
2015-05-07 11:03:29 -0400

    support buffer, view, clamped uint8

--- test.js
+++ test.js
@@ -13,6 +13,9 @@ test('creates the indices for a quad mesh (two triangles)', function(t) {
     t.deepEqual(quad({ type: 'uint8' }), 
       new Uint8Array([0, 1, 2, 0, 2, 3]), 
       'dtype uint8')
+    t.deepEqual(quad({ type: 'uint8_clamped' }), 
+      new Uint8ClampedArray([0, 1, 2, 0, 2, 3]), 
+      'dtype uint8_clamped')
     t.deepEqual(quad(2), 
       new Uint16Array([0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7]), 
       'accepts number')
